64e443a48370b2d40865c06a2d3427610a5ccb41
Samuel Hunag <lspss95207@gmail.com>
2021-12-04 18:52:56 -0600

    Add part3 Lab3 files

---
+++ Part3/Lab3/main.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+
+class Question1(object):
+    def pca_reduce_dimen(self, X):
+        out = None
+        return out
+
+    def pca_project(self, X, k):
+        filtered = None
+        return filtered
+
+
+class Question2(object):
+    def wiener_filter(self, data_noisy, C, mu, sigma):
+        filtered = None
+        return filtered
+
+
+class Question3(object):
+    def embedding(self, A):
+        eig_vec = None
+        eig_val = None
+        return eig_vec, eig_val
