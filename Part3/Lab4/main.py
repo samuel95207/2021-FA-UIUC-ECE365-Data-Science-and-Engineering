5ab86e4e19d255233cfe9babc22de6c8e820c53d
Samuel Hunag <lspss95207@gmail.com>
2021-12-06 01:41:10 -0600

    Add Part3 Lab4 files

---
+++ Part3/Lab4/main.py
@@ -0,0 +1,23 @@
+import numpy as np
+from skimage.transform import radon, iradon, resize
+
+
+class Question1(object):
+    def complete_ortho_matrix(self, M):
+        orth_mat = None
+        return orth_mat
+
+    def recover_ortho_matrix(self, M):
+        orth_mat = None
+        return orth_mat
+
+    def comp_rec_ortho_matrix(self, M):
+        orth_mat = None
+        return orth_mat
+
+
+class Question2(object):
+    def template_matching(self, noisy_proj, I0, M, Tmax):
+        theta = None
+        I_rec = None
+        return I_rec, theta
