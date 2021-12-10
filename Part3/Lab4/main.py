bf4289efd385d5a2657798435d4f91bf260abfa3
Samuel Hunag <lspss95207@gmail.com>
2021-12-09 18:57:55 -0600

    Finish Part3 Lab4

--- Part3/Lab4/main.py
+++ Part3/Lab4/main.py
@@ -1,23 +1,36 @@
 import numpy as np
 from skimage.transform import radon, iradon, resize
+import scipy
 
 
 class Question1(object):
     def complete_ortho_matrix(self, M):
-        orth_mat = None
+        cross = np.cross(M[:, 0], M[:, 1]).reshape((3, 1))
+        orth_mat = np.concatenate((M, cross), axis=1)
         return orth_mat
 
     def recover_ortho_matrix(self, M):
-        orth_mat = None
+        U, S, vh = np.linalg.svd(M)
+        orth_mat = U @ vh
         return orth_mat
 
     def comp_rec_ortho_matrix(self, M):
-        orth_mat = None
+        orth_mat = self.complete_ortho_matrix(M)
+        orth_mat = self.recover_ortho_matrix(orth_mat)
         return orth_mat
 
 
 class Question2(object):
     def template_matching(self, noisy_proj, I0, M, Tmax):
-        theta = None
-        I_rec = None
+        theta = np.linspace(0, 360, M)
+        
+        for t in range(Tmax):
+            radonResult = radon(I0, theta=theta)
+            corr = (noisy_proj.T @ radonResult) / np.sqrt(np.diagonal(radonResult.T @ radonResult))
+            idx = np.argmax(corr, axis=1)
+            theta_best = theta[idx]
+            I0 = iradon(noisy_proj, theta=theta_best)
+
+        theta = theta_best
+        I_rec = I0
         return I_rec, theta
